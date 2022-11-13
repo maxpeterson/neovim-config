5d50c9c4ce1e58b8ded53bb5829067f819c70a72
Max Peterson <mail@maxpeterson.co.uk>
2022-11-13 21:51:08 +0000

    Copy config from dotfiles/vim

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -8,12 +8,17 @@ g.mapleader = ","
 g.maplocalleader = ","
 
 g.taboo_tab_format = ' %N:%P '
+g.argwrap_tail_comma = 1
+g.argwrap_padded_braces = '{'
 
 opt.termguicolors = true -- Enable colors in terminal
 opt.hlsearch = true --Set highlight on search
 opt.number = true --Make line numbers default
 opt.relativenumber = true --Make relative number default
 opt.expandtab = true -- Tabs are space
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
 opt.splitbelow = true -- Set preview window to appear at bottom
 opt.mouse = "a" --Enable mouse mode
 opt.breakindent = true --Enable break indent
@@ -25,8 +30,19 @@ opt.signcolumn = "yes" -- Always show sign column
 opt.clipboard = "unnamedplus" -- Access system clipboard
 opt.wildignore:append "**/node_modules/*"
 opt.wildignore:append "**/.git/*"
+opt.wildignore:append "*.swp"
+opt.wildignore:append "*.bak"
+opt.wildignore:append ".pyc"
+opt.wildignore:append "*.class"
+opt.wildignore:append ".jar"
+opt.wildignore:append "*.gif"
+opt.wildignore:append "*.png"
+opt.wildignore:append "*.jpg"
+opt.wildignore:append "*.dcm"
 opt.winbar = "%{%v:lua.require'config.winbar'.get_winbar()%}"
 opt.showtabline = 2
+opt.spell = true
+opt.backupdir = './_backup,/tmp,.'
 
 -- Highlight on yank
 vim.cmd [[
@@ -34,4 +50,8 @@ vim.cmd [[
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup end
+  autocmd BufEnter *.md exe 'noremap <leader>p :!open -a "Google Chrome.app" %:p<CR>'
+  syntax on
+  filetype on
+  filetype plugin indent on
 ]]
